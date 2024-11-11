db16f2942836094bb9b281fd53aecc2a88a77730
Nice <hoangkimc93@gmail.com>
2024-11-11 16:00:22 +0700

    feat(menu): add styling and fonts

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 const Logo: React.FC = () => {
   return (
     <Flex className="flex-row gap-1 items-center justify-center">
-      <img src="/logo-icon.png" height={36} />
-      <img src="/logo-text.png" height={16} />
+      <img src="/logo-icon.svg" height={36} />
+      <img src="/logo-text.svg" height={16} />
     </Flex>
   );
 };
