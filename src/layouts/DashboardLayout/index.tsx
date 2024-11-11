0ffaca76dfd28316cd762414beb6836d6973d907
Nice <hoangkimc93@gmail.com>
2024-11-11 14:50:14 +0700

    feat(layout): layout sectioning

--- src/layouts/DashboardLayout/index.tsx
+++ src/layouts/DashboardLayout/index.tsx
@@ -2,22 +2,15 @@ import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
 import { path } from '@/constants/path';
+import { Flex } from 'antd/lib';
 
 const DashboardLayout: React.FC = () => {
   return (
-    <div className="flex flex-col min-h-[100vh]">
-      <div className="px-12 bg-neutral-3 h-20 text-white flex flex-row items-center justify-between sticky top-0 left-0 z-10">
-        <Link to={path.HOMEPAGE} className="text-heading-4">
-          LOGO
-        </Link>
-      </div>
-      <div className="border-2 rounded-xl min-h-[calc(100vh-160px)] h-fit max-w-7xl w-11/12 mx-auto my-4">
-        <Outlet />
-        <div>Children route</div>
-      </div>
-      <div className="px-12 bg-neutral-3 h-12 w-full text-white flex flex-row items-center justify-center">
-        <span className="text-heading-5">FOOTER</span>
-      </div>
+    <div className="flex flex-row min-h-[100vh]">
+      <aside className='flex flex-col bg-ebony-10 w-56'></aside>
+      <Flex className='flex-col'>
+        <header></header>
+      </Flex>
     </div>
   );
 };
